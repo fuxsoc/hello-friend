6fba7dd8bd89a543a34977fa26861a1e6ea2e1f6
fuxsoc <sam.sepio.95@protonmail.com>
2020-01-04 23:14:31 +0000

    Friend...that's lame. This will have to do before I give you a better one

---
+++ annonymize.py
@@ -0,0 +1,57 @@
+#!/bin/python
+
+import random
+import requests
+from bs4 import BeautifulSoup
+
+class Annonymize:
+	def __init__(self):
+		self.proxy_list_url = "https://free-proxy-list.net/"
+		self.proxy_list = list()
+
+	def send_get_request(self):
+		self.response = requests.get(self.proxy_list_url)
+
+	def parse_html(self):
+		self.parser = BeautifulSoup(
+			markup = self.response.text,
+			features = "lxml"
+		)
+
+	def get_table(self):
+		self.table = self.parser.find_all("tr")
+
+	def get_cells(self):
+		self.cells = self.row.find_all("td")
+
+	def check_if_secure(self):
+		self.is_secure = self.row.find("td", {"class":"hx"}).text.strip()
+
+	def get_elite_proxies(self):
+		for row in self.table:
+			self.row = row
+			if "elite proxy" in row.text:
+				self.get_cells()
+				self.check_if_secure()
+				if self.is_secure == "yes":
+					proxy_ip = self.cells[0].text.strip()
+					proxy_port = self.cells[1].text.strip()
+					proxy = ':'.join([proxy_ip, proxy_port])
+					self.proxy_list.append(proxy)
+
+	def get_user_agent(self):
+		with open("user_agents.txt", "r") as FILE:
+			user_agents = FILE.readlines()
+		self.user_agent = random.choice(user_agents).strip()
+	
+	def get_proxies(self):
+		self.send_get_request()
+		self.parse_html()
+		self.get_table()
+		self.get_elite_proxies()
+		self.get_user_agent()
+		
+		
+		
+
+
